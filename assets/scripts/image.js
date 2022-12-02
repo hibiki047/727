3d4fe5d5370755103a9ced3066776c965e5c33e1
hibiki047 <75331048+hibiki047@users.noreply.github.com>
2022-12-02 22:26:33 +0800

    less thing
    will add if i find lonte

---
+++ assets/scripts/image.js
@@ -0,0 +1,12 @@
+// here we fucking go again
+image_generate_data = [
+  {
+    name: 'arissa-bd',
+    url: 'https://cdn.discordapp.com/attachments/696405704134885469/929045243997880350/Arissa_2022_bd_gift.png',
+  },
+  {
+    name: 'fireredz-bd',
+    url: 'https://cdn.discordapp.com/attachments/696405704134885469/1018163813712924793/junko_-_fireredz_bd_gift.png',
+  },
+  ]
+
