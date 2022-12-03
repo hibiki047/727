d8dde96f75f1bf1174f82f3752d64144515b6f74
FireRedz <fireredz69420@gmail.com>
2022-12-03 19:35:08 +0800

    Gallery: Make it responsive

--- assets/scripts/image.js
+++ assets/scripts/image.js
@@ -2,13 +2,13 @@
 image_generate_data = [
   {
     name: 'arissa-bd',
-    url: 'https://cdn.discordapp.com/attachments/696405704134885469/929045243997880350/Arissa_2022_bd_gift.png',
+    url: 'https://cdn.discordapp.com/attachments/696405704134885469/929045243997880350/Arissa_2022_bd_gift.png'
   },
   {
     name: 'fireredz-bd',
-    url: 'https://cdn.discordapp.com/attachments/696405704134885469/1018163813712924793/junko_-_fireredz_bd_gift.png',
-  },
-  ]
+    url: 'https://cdn.discordapp.com/attachments/696405704134885469/1018163813712924793/junko_-_fireredz_bd_gift.png'
+  }
+]
 
 function image_gene () {
   var grid = document.getElementById('grid-image')
@@ -17,8 +17,7 @@ function image_gene () {
     imgur = image_generate_data[i]
     grid.innerHTML += `
 <div class="grid-entry">
-    <img class="image"
-        src="${imgur.url}">
+    <img class="image" src="${imgur.url}">
 </div>`
   }
 }
