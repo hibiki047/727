b76ee2af936f98d05039b156052cb7b07dd267ab
hibiki047 <75331048+hibiki047@users.noreply.github.com>
2022-12-04 15:26:09 +0800

    update content

--- assets/scripts/image.js
+++ assets/scripts/image.js
@@ -7,6 +7,22 @@ image_generate_data = [
   {
     name: 'fireredz-bd',
     url: 'https://cdn.discordapp.com/attachments/696405704134885469/1018163813712924793/junko_-_fireredz_bd_gift.png'
+  },
+  {
+    name: 'yuri-shorthair',
+    url: 'https://cdn.discordapp.com/attachments/738853033210806418/1048860118390939689/94258525_2617005165076731_1106995032763662336_n.png'
+  },
+  {
+    name: 'senko-best',
+    url: 'https://cdn.discordapp.com/attachments/738853033210806418/1048860919293280296/best_of_senko_moment.jpg'
+  },
+  {
+    name: 'daughter-ke',
+    url: 'https://cdn.discordapp.com/attachments/738853033210806418/1048861479023169567/best_daughter_xd.jpg'
+  },
+  {
+    name: 'asperl-bd',
+    url: 'https://cdn.discordapp.com/attachments/738853033210806418/1048862175722217532/asperl_bd_gift.jpg'
   }
 ]
 
